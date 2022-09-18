f44b40c63b9bf5e0c2b7e95b3ab921e620e64fd6
Alston Chan <51868695+AlstonChan@users.noreply.github.com>
2022-09-18 11:05:33 +0800

    Update supabase.js

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -10,6 +10,6 @@ const options = {
 
 // Create a single supabase client for interacting with your database
 export const supabase = createClient(
-  process.env.SUPABASE_URL,
-  process.env.SUPABASE_PUBLIC_ANON_KEY
+  "https://snrxmggiuferssrzuzwh.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNucnhtZ2dpdWZlcnNzcnp1endoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTk3ODk3MTQsImV4cCI6MTk3NTM2NTcxNH0.d-TbYaOUZJGqUWZGCj4YYp5JNUi4wCt1C2oUPSTkV7I"
 );
